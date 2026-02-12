0e844473c5def2cc985023a005658258e0150507
Malcolm Schonfield <malcolm.schonfield@oecd.org>
2026-02-12 16:05:10 +0100

    header parsing
    +titles
    +H continuations with +:

--- src/parse/getMetadata.js
+++ src/parse/getMetadata.js
@@ -1,13 +1,16 @@
 const { normaliseKey } = require("../manipulator");
 
 /**
- * Extracts data in the ABC header T R C M K S F D N fields
+ * Extracts data in the ABC _header_ T R C M K S F D N H fields
  * and returns it in a object with properties: title, rhythm, composer, meter, key,
- * source, url, recording, and comments.
+ * source, url, recording, comments, and hComments.
  * Minimal parsing, but a few features:
  * - only extracts the first T title; subsequent T entries are ignored
  * - the key is normalised, so C, Cmaj, C maj, C major will all map to key:"C major"
- * - the comments go in an array, with one array entry per N: line.
+ * - the comments (i.e. the N / notes) go in an array called `comments`, with one array entry per N: line
+ * - the history (H) lines are joined up with spaces into a single line that is returned as `hComments`
+ * - the field continuation `+:` is handled only for lines following an initial H (history)
+ * - if there’s more than one T (title), then titles after the first one are returned in an array `titles`
  * @param {*} abc
  * @returns {object} - The header info
  */
@@ -15,34 +18,46 @@ function getMetadata(abc) {
 	const lines = abc.split("\n"),
 		metadata = {},
 		comments = [],
-		hComments = [];
+		hComments = [],
+		titles = [];
+
+	let currentHeader = "";
 
 	for (const line of lines) {
 		const trimmed = line.trim();
-		if (trimmed.startsWith("T:") && !metadata.title) {
-			metadata.title = trimmed.substring(2).trim();
+		const trimmed2 = trimmed.substring(2).trim().replace(/%.+/, "");
+		if (trimmed.startsWith("T:")) {
+			if (!metadata.title) metadata.title = trimmed2;
+			else titles.push(trimmed2);
 		} else if (trimmed.startsWith("R:")) {
-			metadata.rhythm = trimmed.substring(2).trim().toLowerCase();
+			metadata.rhythm = trimmed2.toLowerCase();
 		} else if (trimmed.startsWith("C:")) {
-			metadata.composer = trimmed.substring(2).trim();
+			metadata.composer = trimmed2;
 		} else if (trimmed.startsWith("M:")) {
-			metadata.meter = trimmed.substring(2).trim();
+			metadata.meter = trimmed2;
 		} else if (trimmed.startsWith("K:")) {
-			metadata.key = normaliseKey(trimmed.substring(2).trim()).join(" ");
+			metadata.key = normaliseKey(trimmed2).join(" ");
 			// metadata.indexOfKey = i
 			break;
 		} else if (trimmed.startsWith("S:")) {
-			metadata.source = trimmed.substring(2).trim();
+			metadata.source = trimmed2;
 		} else if (trimmed.startsWith("O:")) {
-			metadata.origin = trimmed.substring(2).trim();
+			metadata.origin = trimmed2;
 		} else if (trimmed.startsWith("F:")) {
-			metadata.url = trimmed.substring(2).trim();
+			metadata.url = trimmed2;
 		} else if (trimmed.startsWith("D:")) {
-			metadata.recording = trimmed.substring(2).trim();
+			metadata.recording = trimmed2;
 		} else if (trimmed.startsWith("N:")) {
-			comments.push(trimmed.substring(2).trim());
+			comments.push(trimmed2);
 		} else if (trimmed.startsWith("H:")) {
-			hComments.push(trimmed.substring(2).trim());
+			currentHeader = "H";
+			hComments.push(trimmed2);
+		} else if (trimmed.startsWith("+:")) {
+			switch (currentHeader) {
+				case "H":
+					hComments.push(trimmed2);
+					break;
+			}
 		}
 	}
 	if (comments.length > 0) {
@@ -51,6 +66,7 @@ function getMetadata(abc) {
 	if (hComments.length > 0) {
 		metadata.hComments = hComments.join(" ");
 	}
+	if (titles.length > 0) metadata.titles = titles;
 
 	return metadata;
 }
